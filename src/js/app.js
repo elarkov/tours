9eaddfa8cad6f7ab7baf23a663393ae708ae0d4f
Evgeniy Larkov <elarkov@inbox.ru>
2022-09-08 15:48:08 +0700

    second first

--- src/js/app.js
+++ src/js/app.js
@@ -4,9 +4,9 @@ import * as sideMenu from "./modules/side-menu.js";
 import * as stickyHeader from "./modules/sticky-header.js";
 import * as slider from "./modules/slider.js";
 import * as tabs from "./modules/tabs.js";
+import * as accordion from "./modules/accordion.js";
 // import * as phoneinput from "./modules/phoneinput.js";
 // import * as renderQuiz from "./modules/quiz.js";
-// 
 // import * as toScroll from "./modules/toScroll.js";
 // import * as spamdetect from "./modules/spamDetect.js";
 // import * as ajaxSend from "./modules/ajax.js";
@@ -23,9 +23,9 @@ document.addEventListener('DOMContentLoaded', function () { //dom is ready
   stickyHeader.stickyHeader();
   slider.renderSlider();
   tabs.renderTabs(document.querySelectorAll('.js-tabs'));
+  accordion.renderAccordion();
   // phoneinput.phoneInput();
   // //renderQuiz.renderQuiz();
-  // accordion.renderAccordion();
   // popup.renderPopup();
   // toScroll.doScroll();
   // spamdetect.spamDetect();
