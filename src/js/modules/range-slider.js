7f630c5c0d31261ec8758f88ad53b4a465f85913
Evgeniy <elarkov@inbox.ru>
2022-09-15 22:46:49 +0700

    add popups

--- src/js/modules/range-slider.js
+++ src/js/modules/range-slider.js
@@ -2,60 +2,95 @@ export function rangeSlider() {
   let currencyLang = '₽';
   let days = ' дня';
   const filterRoot = document.querySelector('.js-filter-range');
+
+ 
   
-  if (filterRoot) {
-    let sliderPrice = document.querySelector('#range-price');
-    let sliderDuration = document.querySelector('#range-duration');
+  // if (filterRoot) {
+    const filterSelectRange = document.querySelectorAll('.filter-select--range');
+    filterSelectRange.forEach(function(item) {
+      let sliderPrice = item.querySelectorAll('.range-price');
+      let sliderDuration = item.querySelectorAll('.range-duration');
+      let valueElement = item.querySelectorAll('.filter-select__inputs-input');
 
-    let valueElement = document.querySelectorAll('.filter-select__inputs-input');
+      valueElement.forEach((input) => {
+        input.oninput = function () {
+          this.value = this.value.replace(/[^\d]/g, '');
+        };
+      });
 
-    const configPrice = {
-      range: {
-        'min': [0],
-        'max': 900000
-      },
-      start: [0, 900000],
-      connect: true,
-      format: wNumb({
-        decimals: 0,
-        thousand: ',',
-        suffix: currencyLang,
-      })
-    };
+      sliderPrice.forEach(function(slider) {
+        const configPrice = {
+          range: {
+            'min': [0],
+            'max': 900000
+          },
+          start: [0, 900000],
+          connect: true,
+          format: wNumb({
+            decimals: 0,
+            thousand: ',',
+            suffix: currencyLang,
+          })
+        };
+        
+        noUiSlider.create(slider, configPrice);
+        slider.noUiSlider.on('update', function (values, handle) {
+          let inputs = Array.prototype.slice.call(valueElement);
+          inputs[handle].value = `${values[handle]}`;
+        });
+      });
 
-    const configDays = {
-      range: {
-        'min': [2],
-        'max': 93
-      },
-      start: [2, 93],
-      connect: true,
-      format: wNumb({
-        decimals: 0,
-        suffix: days,
-      })
-    }
 
-    valueElement.forEach((el) => {
-      el.oninput = function () {
-        this.value = this.value.replace(/[^\d]/g, '');
-      };
+      sliderDuration.forEach(function(slider) {
+        const configDays = {
+          range: {
+            'min': [2],
+            'max': 93
+          },
+          start: [2, 93],
+          connect: true,
+          format: wNumb({
+            decimals: 0,
+            suffix: days,
+          })
+        }
+        
+        noUiSlider.create(slider, configDays);
+        slider.noUiSlider.on('update', function (values, handle) {
+          let inputs = Array.prototype.slice.call(valueElement);
+          inputs[handle].value = `${values[handle]}`;
+        });
+      });
     });
+    
+    // let sliderDuration = document.querySelector('#range-duration');
 
-    noUiSlider.create(sliderPrice, configPrice);
-    noUiSlider.create(sliderDuration, configDays);
+    
 
-    sliderPrice.noUiSlider.on('update', function (values, handle) {
-      let inputs = Array.prototype.slice.call(valueElement);
-      inputs[handle].value = `${values[handle]}`;
-    });
+    
 
-    sliderDuration.noUiSlider.on('update', function (values, handle) {
-      const durationInputOne = valueElement[2];
-      const durationInputSecond = valueElement[3];
-      const inputsDuration = [durationInputOne, durationInputSecond];
-      inputsDuration[handle].value = `${values[handle]}`;
-    });
-  }
+    
+    
+  
+
+    
+
+    
+
+    
+    // noUiSlider.create(sliderDuration, configDays);
+
+    // sliderPrice.noUiSlider.on('update', function (values, handle) {
+    //   let inputs = Array.prototype.slice.call(valueElement);
+    //   inputs[handle].value = `${values[handle]}`;
+    // });
+
+    // sliderDuration.noUiSlider.on('update', function (values, handle) {
+    //   const durationInputOne = valueElement[2];
+    //   const durationInputSecond = valueElement[3];
+    //   const inputsDuration = [durationInputOne, durationInputSecond];
+    //   inputsDuration[handle].value = `${values[handle]}`;
+    // });
+  // }
 
 }
